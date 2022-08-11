14214d7e3ea500a716f9102ae9637c6842f338c3
murilo muller <murilomuller@protonmail.com>
2022-08-11 09:08:05 -0300

    botão random finalizado

--- 50-project-in-50-day/14/script.js
+++ 50-project-in-50-day/14/script.js
@@ -1,15 +1,25 @@
 const tagsEl = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
 
-
 textarea.focus()
 
-textarea.addEventListener('keyup', (e) => {
+textarea.addEventListener('keyup', e => {
   createTags(e.target.value)
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+
+    randomSelect()
+  }
 })
 
 function createTags(input) {
-  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+  const tags = input
+    .split(',')
+    .filter(tag => tag.trim() !== '')
+    .map(tag => tag.trim())
 
   tagsEl.innerHTML = ''
 
@@ -19,5 +29,41 @@ function createTags(input) {
     tagEl.innerText = tag
     tagsEl.appendChild(tagEl)
   })
+}
 
+function randomSelect() {
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highLightTag(randomTag)
+
+    setTimeout(() => {
+      unHighLightTag(randomTag)
+    })
+  }, 100)
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+
+      highLightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highLightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighLightTag(tag) {
+  tag.classList.remove('highlight')
 }
