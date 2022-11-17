6972ed9eacbf64158e3df50f52ee260f990d7ee4
Murilo Muller <murilomuller@protonmail.com>
2022-11-17 19:39:36 -0300

    cups function toggle add

--- 50-project-in-50-day/17-Drink-water/script.js
+++ 50-project-in-50-day/17-Drink-water/script.js
@@ -8,5 +8,18 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highLightCups(idx) {
-  console.log(idx)
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full')
+    } else {
+      cup.classList.remove('full')
+    }
+  })
 }
